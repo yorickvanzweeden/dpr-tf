5133714b9ea9b088378d1e94517fbb4d8c6a70c6
Yorick van Zweeden <git@yorickvanzweeden.nl>
2022-04-22 20:06:51 +0200

    Clean up unneeded code

--- src/dense_passage_retriever.py
+++ src/dense_passage_retriever.py
@@ -20,85 +20,10 @@ Original Paper [link](https://arxiv.org/pdf/2004.04906.pdf)
 Install `transformers`, `faiss-cpu` via `pip install -q transformers faiss-cpu``.
 """
 
-import os
-import json
-import faiss
-import random
-import numpy as np
-import pandas as pd
 import tensorflow as tf
 from tensorflow import keras
-from tqdm import tqdm_notebook
-from dataclasses import dataclass
 from tensorflow.keras import layers
-from transformers import AutoTokenizer
-from transformers import AutoTokenizer, TFAutoModel
-
-
-"""
-## Load and Preprocess Dataset
-"""
-
-
-def encode_query_passage(tokenizer, dicts, model_config, data_config):
-    """Encode Text i.e. queries and passages into token_ids"""
-
-    passage_input_ids = []
-    passage_token_type_ids = []
-    passage_attention_mask = []
-
-    queries = []
-    for i in tqdm_notebook(range(len(dicts))):
-        di = dicts[i]
-        di_query = di["query"]
-        di_passages = di["passages"]
-        di_positives = [
-            (pi["title"], pi["text"]) for pi in di_passages if pi["label"] == "positive"
-        ]
-        di_negatives = [
-            (ni["title"], ni["text"])
-            for ni in di_passages
-            if ni["label"] == "hard_negative"
-        ]
-
-        if data_config.num_positives == len(
-            di_positives
-        ) and data_config.num_hard_negatives == len(di_negatives):
-
-            queries.append(di_query)
-            i_passages = di_positives + di_negatives
-            i_passage_inputs = tokenizer.batch_encode_plus(
-                i_passages,
-                max_length=model_config.passage_max_seq_len,
-                add_special_tokens=True,
-                truncation=True,
-                truncation_strategy="longest_first",
-                padding="max_length",
-                return_token_type_ids=True,
-            )
-            passage_input_ids.append(np.array(i_passage_inputs["input_ids"]))
-            passage_token_type_ids.append(np.array(i_passage_inputs["token_type_ids"]))
-            passage_attention_mask.append(np.array(i_passage_inputs["attention_mask"]))
-
-    query_inputs = tokenizer.batch_encode_plus(
-        queries,
-        max_length=model_config.query_max_seq_len,
-        add_special_tokens=True,
-        truncation=True,
-        truncation_strategy="longest_first",
-        padding="max_length",
-        return_token_type_ids=True,
-        return_tensors="np",
-    )
-
-    return {
-        "query_input_ids": query_inputs["input_ids"],
-        "query_token_type_ids": query_inputs["token_type_ids"],
-        "query_attention_mask": query_inputs["attention_mask"],
-        "passage_input_ids": np.array(passage_input_ids),
-        "passage_token_type_ids": np.array(passage_token_type_ids),
-        "passage_attention_mask": np.array(passage_attention_mask),
-    }
+from transformers import TFAutoModel
 
 
 """
@@ -109,10 +34,10 @@ def encode_query_passage(tokenizer, dicts, model_config, data_config):
 class QueryModel(tf.keras.Model):
     """Query Model"""
 
-    def __init__(self, model_config, **kwargs):
+    def __init__(self, model_config, from_pt=False, **kwargs):
         super().__init__(**kwargs)
         # Load Pretrained models
-        self.query_encoder = TFAutoModel.from_pretrained(model_config.model_name)
+        self.query_encoder = TFAutoModel.from_pretrained(model_config.model_name, from_pt=from_pt)
         # Add dropout layer
         self.dropout = layers.Dropout(model_config.dropout)
 
@@ -126,10 +51,10 @@ class QueryModel(tf.keras.Model):
 class PassageModel(tf.keras.Model):
     """Passage Model"""
 
-    def __init__(self, model_config, **kwargs):
+    def __init__(self, model_config, from_pt=False, **kwargs):
         super().__init__(**kwargs)
         # Load Pretrained models
-        self.passage_encoder = TFAutoModel.from_pretrained(model_config.model_name)
+        self.passage_encoder = TFAutoModel.from_pretrained(model_config.model_name, from_pt=from_pt)
         # Add dropout layer
         self.dropout = layers.Dropout(model_config.dropout)
 
@@ -140,14 +65,14 @@ class PassageModel(tf.keras.Model):
         return pooled_output
 
 
-def cross_replica_concat(values, v_shape):
+def cross_replica_concat(values):
 
     context = tf.distribute.get_replica_context()
     gathered = context.all_gather(values, axis=0)
 
     return tf.roll(
       gathered,
-      -context.replica_id_in_sync_group * values.shape[0], #v_shape,#values.shape[0],
+      -context.replica_id_in_sync_group * values.shape[0],
       axis=0
     )
 
@@ -260,8 +185,8 @@ class BiEncoderModel(tf.keras.Model):
             query_embeddings = self.query_forward(X)
 
             # Get all replica concat values for In-Batch loss calculation
-            global_passage_embeddings = cross_replica_concat(passage_embeddings, 32)
-            global_query_embeddings = cross_replica_concat(query_embeddings, 16)
+            global_passage_embeddings = cross_replica_concat(passage_embeddings)
+            global_query_embeddings = cross_replica_concat(query_embeddings)
 
             # Dot product similarity
             similarity_scores = tf.linalg.matmul(
@@ -284,8 +209,8 @@ class BiEncoderModel(tf.keras.Model):
         query_embeddings = self.query_forward(X)
 
         # Get all replica concat values for In-Batch loss calculation
-        global_passage_embeddings = cross_replica_concat(passage_embeddings, 32)
-        global_query_embeddings = cross_replica_concat(query_embeddings, 16)
+        global_passage_embeddings = cross_replica_concat(passage_embeddings)
+        global_query_embeddings = cross_replica_concat(query_embeddings)
 
         # Dot product similarity
         similarity_scores = tf.linalg.matmul(
@@ -302,113 +227,3 @@ class BiEncoderModel(tf.keras.Model):
     @property
     def metrics(self):
         return [self.loss_tracker]
-
-
-"""
-## Model Evaluation
-"""
-
-
-def combine_title_context(titles, texts):
-    res = []
-    for title, ctx in zip(titles, texts):
-        if title is None:
-            title = ""
-        res.append(tuple((title, ctx)))
-    return res
-
-
-def process_single_example(passages):
-    answer_index = -1
-    titles = []
-    texts = []
-    for i in range(len(passages)):
-        p = passages[i]
-        titles.append(p["title"])
-        texts.append(p["text"])
-        if p["label"] == "positive":
-            answer_index = i
-
-    res = combine_title_context(titles, texts)
-
-    return res, answer_index
-
-
-def process_examples(dicts):
-    processed_passages = []
-    queries = []
-    answer_indexes = []
-    global_answer_index = 0
-
-    for i in range(len(dicts)):
-        dict_ = dicts[i]
-        query = dict_["query"]
-        queries.append(query)
-
-        passages = dict_["passages"]
-        res, answer_index = process_single_example(passages)
-
-        i_answer_index = global_answer_index + answer_index
-
-        processed_passages.extend(res)
-        answer_indexes.append(i_answer_index)
-
-        global_answer_index = global_answer_index + len(passages)
-    return queries, answer_indexes, processed_passages
-
-
-def extracted_passage_embeddings(tokenizer, passage_encoder, processed_passages, model_config):
-    """Extract Passage Embeddings"""
-    passage_inputs = tokenizer.batch_encode_plus(
-        processed_passages,
-        add_special_tokens=True,
-        truncation=True,
-        padding="max_length",
-        max_length=model_config.passage_max_seq_len,
-        return_token_type_ids=True,
-    )
-    passage_embeddings = passage_encoder.predict(
-        [
-            np.array(passage_inputs["input_ids"]),
-            np.array(passage_inputs["attention_mask"]),
-            np.array(passage_inputs["token_type_ids"]),
-        ],
-        batch_size=512,
-        verbose=1,
-    )
-    return passage_embeddings
-
-
-def extracted_query_embeddings(tokenizer, query_encoder, queries, model_config):
-    """Extract Query Embeddings"""
-    query_inputs = tokenizer.batch_encode_plus(
-        queries,
-        add_special_tokens=True,
-        truncation=True,
-        padding="max_length",
-        max_length=model_config.query_max_seq_len,
-        return_token_type_ids=True,
-    )
-    query_embeddings = query_encoder.predict(
-        [
-            np.array(query_inputs["input_ids"]),
-            np.array(query_inputs["attention_mask"]),
-            np.array(query_inputs["token_type_ids"]),
-        ],
-        batch_size=512,
-        verbose=1,
-    )
-    return query_embeddings
-
-
-def get_k_accuracy(faiss_index, query_embeddings, answer_indexes, k):
-
-    prob, index = faiss_index.search(query_embeddings, k=k)
-
-    corrects = []
-    for i in tqdm_notebook(range(len(answer_indexes))):
-        i_index = index[i]
-        i_count = len(np.where(i_index == answer_indexes[i])[0])
-        if i_count > 0:
-            corrects.append((i, answer_indexes[i]))
-    return corrects
