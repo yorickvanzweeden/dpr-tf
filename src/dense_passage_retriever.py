412ce91097707345e6dd395028a42a7741f64130
Yorick van Zweeden <git@yorickvanzweeden.nl>
2022-04-25 14:43:57 +0200

    Remove passage reference

--- src/dense_passage_retriever.py
+++ src/dense_passage_retriever.py
@@ -20,7 +20,6 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from transformers import TFAutoModel
 
-
 """
 ## Model Preparation
 """
@@ -37,38 +36,19 @@ class QueryModel(tf.keras.Model):
         self.dropout = layers.Dropout(model_config.dropout)
 
     def call(self, inputs, training=False, **kwargs):
-
         pooled_output = self.query_encoder(inputs, training=training, **kwargs)[1]
         pooled_output = self.dropout(pooled_output, training=training)
         return pooled_output
 
 
-class PassageModel(tf.keras.Model):
-    """Passage Model"""
-
-    def __init__(self, model_config, from_pt=False, **kwargs):
-        super().__init__(**kwargs)
-        # Load Pretrained models
-        self.passage_encoder = TFAutoModel.from_pretrained(model_config.model_name, from_pt=from_pt)
-        # Add dropout layer
-        self.dropout = layers.Dropout(model_config.dropout)
-
-    def call(self, inputs, training=False, **kwargs):
-
-        pooled_output = self.passage_encoder(inputs, training=training, **kwargs)[1]
-        pooled_output = self.dropout(pooled_output, training=training)
-        return pooled_output
-
-
 def cross_replica_concat(values):
-
     context = tf.distribute.get_replica_context()
     gathered = context.all_gather(values, axis=0)
 
     return tf.roll(
-      gathered,
-      -context.replica_id_in_sync_group * values.shape[0],
-      axis=0
+        gathered,
+        -context.replica_id_in_sync_group * values.shape[0],
+        axis=0
     )
 
 
@@ -76,31 +56,24 @@ class BiEncoderModel(tf.keras.Model):
     """Bi-Encoder Query & Passage Model"""
 
     def __init__(
-        self,
-        query_encoder,
-        passage_encoder,
-        num_passages_per_question,
-        model_config,
-        strategy,
-        global_batch_size,
-        *args,
-        **kwargs
+            self,
+            query_encoder,
+            model_config,
+            strategy,
+            global_batch_size,
+            *args,
+            **kwargs
     ):
         super().__init__(*args, **kwargs)
 
         # Query encoder model
         self.query_encoder = query_encoder
-        # Passage encoder model
-        self.passage_encoder = passage_encoder
-        # No. positives plus No. of hard negatives
-        self.num_passages_per_question = num_passages_per_question
         # Model configuration
         self.model_config = model_config
         # Execution strategy (colab)
         self.strategy = strategy
         # Loss tracker
         self.loss_tracker = keras.metrics.Mean(name="loss")
-        self.loss_typo_tracker = keras.metrics.Mean(name="loss_typo")
         # Define loss
         self.loss_fn = keras.losses.SparseCategoricalCrossentropy(
             reduction=keras.losses.Reduction.NONE, from_logits=True
@@ -110,42 +83,10 @@ class BiEncoderModel(tf.keras.Model):
 
     def calculate_loss(self, logits):
         """Function to calculate in batch loss"""
-        # Make In-Batch Labels:
-        # Given single quetion positives are placed first followed by negatives.
-        labels = tf.convert_to_tensor(
-            [
-                i
-                for i in range(
-                    0,
-                    (self.global_batch_size * self.num_passages_per_question),
-                    self.num_passages_per_question,
-                )
-            ]
-        )
-
+        labels = tf.convert_to_tensor([i for i in range(0, self.global_batch_size)])
         loss = self.loss_fn(labels, logits)
         scale_loss = tf.reduce_sum(loss) * (1.0 / self.global_batch_size)
         return scale_loss
-
-    def passage_forward(self, X):
-
-        # Reshape input (BS, num_passages_per_question, seq_len) -> (BS*num_passages_per_question, seq_len)
-        input_shape = (
-            self.model_config.batch_size_per_replica * self.num_passages_per_question,
-            self.model_config.passage_max_seq_len,
-        )
-        input_ids = tf.reshape(X["passage_input_ids"], input_shape)
-        attention_mask = tf.reshape(X["passage_attention_mask"], input_shape)
-        token_type_ids = tf.zeros(shape=input_shape, dtype=tf.int32)
-
-        input_ids = tf.cast(input_ids, tf.int32)
-        attention_mask = tf.cast(attention_mask, tf.int32)
-
-        # Call passage encoder model
-        outputs = self.passage_encoder(
-            [input_ids, attention_mask, token_type_ids], training=True
-        )
-        return outputs
 
     def query_forward(self, X, prefix="query"):
         # Reshape input (BS, seq_len) -> (BS, seq_len)
@@ -159,58 +100,48 @@ class BiEncoderModel(tf.keras.Model):
 
         input_ids = tf.cast(input_ids, tf.int32)
         attention_mask = tf.cast(attention_mask, tf.int32)
-        
+
         outputs = self.query_encoder(
             [input_ids, attention_mask, token_type_ids], training=True
         )
         return outputs
 
     def train_step(self, X):
-
         with tf.GradientTape() as tape:
             # Call encoder models
-            passage_embeddings = self.passage_forward(X)
             query_embeddings = self.query_forward(X, prefix="query")
             typo_embeddings = self.query_forward(X, prefix="typo")
 
             # Get all replica concat values for In-Batch loss calculation
-            global_passage_embeddings = cross_replica_concat(passage_embeddings)
             global_query_embeddings = cross_replica_concat(query_embeddings)
             global_typo_embeddings = cross_replica_concat(typo_embeddings)
 
             # Dot product similarity
             similarity_scores = tf.linalg.matmul(
-                global_query_embeddings, global_passage_embeddings, transpose_b=True
-            )
-
-            typo_scores = tf.linalg.matmul(
                 global_query_embeddings, global_typo_embeddings, transpose_b=True
             )
 
-            loss_orig = self.calculate_loss(similarity_scores) / self.strategy.num_replicas_in_sync
-            loss_typo = self.calculate_loss(typo_scores) / self.strategy.num_replicas_in_sync
-            loss = loss_orig + loss_typo
+            loss = self.calculate_loss(similarity_scores) / self.strategy.num_replicas_in_sync
 
         # Backward pass
         gradients = tape.gradient(loss, self.trainable_variables)
         self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
         # Monitor loss
         self.loss_tracker.update_state(loss)
-        self.loss_typo_tracker.update_state(loss_typo)
-        return {"loss": self.loss_tracker.result(), "loss_typo": self.loss_typo_tracker.result()}
+        return {"loss": self.loss_tracker.result()}
 
     def test_step(self, X):
         # Call encoder models
-        passage_embeddings = self.passage_forward(X)
-        query_embeddings = self.query_forward(X)
+        query_embeddings = self.query_forward(X, prefix="query")
+        typo_embeddings = self.query_forward(X, prefix="typo")
 
         # Get all replica concat values for In-Batch loss calculation
-        global_passage_embeddings = cross_replica_concat(passage_embeddings)
         global_query_embeddings = cross_replica_concat(query_embeddings)
+        global_typo_embeddings = cross_replica_concat(typo_embeddings)
 
         # Dot product similarity
         similarity_scores = tf.linalg.matmul(
-            global_query_embeddings, global_passage_embeddings, transpose_b=True
+            global_query_embeddings, global_typo_embeddings, transpose_b=True
         )
 
         loss = self.calculate_loss(similarity_scores)
@@ -222,4 +153,4 @@ class BiEncoderModel(tf.keras.Model):
 
     @property
     def metrics(self):
-        return [self.loss_tracker, self.loss_typo_tracker]
+        return [self.loss_tracker]
